cb1455e36f82ffcfb2834ab2c92361a39223cf12
Yaçin <iamyacin>
2022-10-15 15:35:36 +0100

    clear local.yaml and add installation for openssh-server

--- prepare-local.sh
+++ prepare-local.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 sudo apt update
+sudo apt install openssh-server
 sudo apt install ansible
 sudo apt install git
 sudo apt install virtualbox
 sudo apt install vagrant
+
+ssh-keygen
 
 ansible-galaxy install -r requirements.yaml
 
