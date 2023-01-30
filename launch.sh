c41dae895b33d00453e6b5c688542356d29bf243
Yaçin <yacin@ElitBook>
2023-01-30 14:19:25 +0100

    update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 sudo apt update
+sudo apt install vim
 sudo apt install openssh-server
 sudo apt install ansible
-sudo apt install git
 sudo apt install virtualbox
 sudo apt install vagrant
 
-ssh-keygen
+#ansible-galaxy install -r requirements.yaml
 
-ansible-galaxy install -r requirements.yaml
-
-vagrant up
+#vagrant up
