ae171e3f0f35805bec6033c2d18b13cbbe0bea54
Yaçin <yassine-daouji@outlook.com>
2024-08-02 23:36:35 +0200

    fix retreave secrets method

--- get-secrets.sh
+++ get-secrets.sh
@@ -1,49 +1,42 @@
-#!/bin/bash
+#!/bin/zsh
 
-# Configuration
-GITLAB_URL="https://gitlab.com"
-PROJECT_ID="40235593"
-JOB_NAME="retrieve_secret"
+source ~/.zshrc
 
-# Access the token from the environment variable
-TOKEN="$GITLAB_TOKEN"
+# List of variable keys you want to retrieve
+VARIABLE_KEYS=("MEGA_EMAIL" "MEGA_PASSWORD")
 
-# Trigger the pipeline
-RESPONSE=$(curl --silent --request POST --form "ref=main" --header "PRIVATE-TOKEN: $TOKEN" "$GITLAB_URL/api/v4/projects/$PROJECT_ID/trigger/pipeline")
-PIPELINE_ID=$(echo "$RESPONSE" | jq --raw-output '.id')
+# Function to get variable value
+get_variable_value() {
+    local variable_key="$1"
+    local response=$(curl --silent --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "https://gitlab.com/api/v4/projects/40235593/variables/$variable_key")
 
-if [ -z "$PIPELINE_ID" ]; then
-  echo "Pipeline ID not found"
-  exit 1
-fi
+    # Check if the response contains an error
+    if echo "$response" | jq -e .error >/dev/null; then
+        echo "Error retrieving variable $variable_key: $(echo $response | jq -r .error)"
+        exit 1
+    fi
+    
+    # Extract the value using jq
+    local variable_value=$(echo "$response" | jq -r '.value')
+    
+    # Check if the variable value was retrieved successfully
+    if [ -z "$variable_value" ]; then
+        echo "Failed to retrieve the value of the variable: $variable_key"
+        exit 1
+    fi
+    echo "$variable_value"
+}
 
-echo "Pipeline triggered with ID: $PIPELINE_ID"
-
-# Wait for the pipeline to complete
-while true; do
-  STATUS=$(curl --silent --header "PRIVATE-TOKEN: $TOKEN" "$GITLAB_URL/api/v4/projects/$PROJECT_ID/pipelines/$PIPELINE_ID" | jq --raw-output '.status')
-  
-  if [ "$STATUS" == "success" ]; then
-    echo "Pipeline succeeded"
-    break
-  elif [ "$STATUS" == "failed" ]; then
-    echo "Pipeline failed"
-    exit 1
-  else
-    echo "Pipeline in progress... Waiting..."
-    sleep 30
-  fi
+# Loop through each variable key and get its value
+for key in "${VARIABLE_KEYS[@]}"; do
+    value=$(get_variable_value "$key")
+    echo "Value retreaved for : $key"
+    # Check if the variable is already in .zshrc
+    if grep -q "^export $key=" ~/.zshrc; then
+        # Replace the existing variable
+        sed -i "s|^export $key=.*|export $key='$value'|" ~/.zshrc
+    else
+        # Append the new variable
+        echo "export $key='$value'" >> ~/.zshrc
+    fi
 done
-
-# Get the Job ID from the pipeline
-JOB_ID=$(curl --silent --header "PRIVATE-TOKEN: $TOKEN" "$GITLAB_URL/api/v4/projects/$PROJECT_ID/pipelines/$PIPELINE_ID/jobs" | jq --raw-output '.[] | select(.name=="'$JOB_NAME'") | .id')
-
-if [ -z "$JOB_ID" ]; then
-  echo "Job ID not found for job name: $JOB_NAME"
-  exit 1
-fi
-
-# Download the artifact
-curl --header "PRIVATE-TOKEN: $TOKEN" --output secret.env "$GITLAB_URL/api/v4/projects/$PROJECT_ID/jobs/$JOB_ID/artifacts/secret.env"
-
-echo "Artifact downloaded successfully"
